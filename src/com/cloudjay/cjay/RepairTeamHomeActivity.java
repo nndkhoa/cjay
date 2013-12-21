639c90ef49d639e07998d42f0a796b50ec0a6b9c
Tieu Bao <nntruonghan@gmail.com>
2013-12-21 11:33:37 +0700

    #3 refactor HomeActivity; role based login; close #3

---
+++ src/com/cloudjay/cjay/RepairTeamHomeActivity.java
@@ -0,0 +1,81 @@
+package com.cloudjay.cjay;
+
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.view.ViewPager;
+import android.support.v4.view.ViewPager.OnPageChangeListener;
+import com.actionbarsherlock.app.ActionBar;
+import com.actionbarsherlock.app.ActionBar.Tab;
+import com.actionbarsherlock.app.ActionBar.TabListener;
+import com.actionbarsherlock.app.SherlockFragmentActivity;
+import com.actionbarsherlock.view.Menu;
+import com.cloudjay.cjay.adapter.ViewPagerAdapter;
+import com.googlecode.androidannotations.annotations.AfterViews;
+import com.googlecode.androidannotations.annotations.EActivity;
+import com.googlecode.androidannotations.annotations.ViewById;
+
+@EActivity(R.layout.activity_gate_home)
+public class RepairTeamHomeActivity extends SherlockFragmentActivity implements
+		OnPageChangeListener, TabListener {
+
+	private String[] locations;
+	@ViewById
+	ViewPager pager;
+
+	@AfterViews
+	void afterViews() {
+		locations = getResources().getStringArray(R.array.locations);
+		configureViewPager();
+		configureActionBar();
+	}
+
+	private void configureViewPager() {
+		ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(
+				getSupportFragmentManager(), locations);
+		pager.setAdapter(viewPagerAdapter);
+		pager.setOnPageChangeListener(this);
+	}
+
+	public void onPageSelected(int position) {
+		Tab tab = getSupportActionBar().getTabAt(position);
+		getSupportActionBar().selectTab(tab);
+	}
+
+	private void configureActionBar() {
+		getSupportActionBar().setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+		for (String location : locations) {
+			Tab tab = getSupportActionBar().newTab();
+			tab.setText(location);
+			tab.setTabListener(this);
+			getSupportActionBar().addTab(tab);
+		}
+	}
+
+	@Override
+	public void onTabSelected(Tab tab, FragmentTransaction ft) {
+		int position = tab.getPosition();
+		pager.setCurrentItem(position);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getSupportMenuInflater();
+		return true;
+	}
+
+	@Override
+	public void onPageScrollStateChanged(int position) {
+	}
+
+	@Override
+	public void onPageScrolled(int position, float positionOffset,
+			int positionOffsetPixels) {
+	}
+
+	@Override
+	public void onTabUnselected(Tab tab, FragmentTransaction ft) {
+	}
+
+	@Override
+	public void onTabReselected(Tab tab, FragmentTransaction ft) {
+	}
+}
