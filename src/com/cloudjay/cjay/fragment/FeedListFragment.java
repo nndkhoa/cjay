5033fcb3ce145af0f18e63c8f36e551bc9962e56
Hector Zhao <long@greengar.com>
2013-12-21 16:58:57 +0700

    #1: add temporary CameraActivity from JayPix app

--- src/com/cloudjay/cjay/fragment/FeedListFragment.java
+++ src/com/cloudjay/cjay/fragment/FeedListFragment.java
@@ -4,31 +4,35 @@ import java.util.ArrayList;
 import java.util.Calendar;
 
 import android.app.Dialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.ListView;
-import android.widget.AdapterView.OnItemClickListener;
 
 import com.actionbarsherlock.app.SherlockDialogFragment;
 import com.ami.fundapter.BindDictionary;
 import com.ami.fundapter.FunDapter;
 import com.ami.fundapter.extractors.StringExtractor;
 import com.ami.fundapter.interfaces.StaticImageLoader;
+import com.cloudjay.cjay.CameraActivity_;
 import com.cloudjay.cjay.R;
 import com.cloudjay.cjay.model.Container;
 import com.googlecode.androidannotations.annotations.EFragment;
-import com.j256.ormlite.logger.Log;
 
 @EFragment(R.layout.fragment_feeds)
-public class FeedListFragment extends SherlockDialogFragment implements OnItemClickListener {
+public class FeedListFragment extends SherlockDialogFragment implements OnClickListener, OnItemClickListener {
 	
 	private final static String TAG = "FeedListFragment";
 	
+	private Button mAddNewBtn;
 	private ListView mFeedListView;
 	private ArrayList<Container> mFeeds;
 
@@ -51,8 +55,10 @@ public class FeedListFragment extends SherlockDialogFragment implements OnItemCl
 			Bundle savedInstanceState) {
 		View view = inflater.inflate(R.layout.fragment_feeds, container, false);
 
+		mAddNewBtn = (Button) view.findViewById(R.id.btn_add_new);
+		mAddNewBtn.setOnClickListener(this);
+		
 		mFeedListView = (ListView) view.findViewById(R.id.feeds);
-
 		mFeedListView.setOnItemClickListener(this);
 		initFunDapter(mFeeds);
 
@@ -71,15 +77,13 @@ public class FeedListFragment extends SherlockDialogFragment implements OnItemCl
 	}
 
 	@Override
-	public void onResume() {
-		super.onResume();
-
-		// Hector: will update UI here
+	public void onClick(View view) {
+		Intent intent = new Intent(getActivity(), CameraActivity_.class);
+		startActivity(intent);
 	}
 
 	@Override
 	public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-		// TODO Auto-generated method stub
 		// Hector: go to details from here
 		android.util.Log.d(TAG, "Show item at position: " + position);
 	}
@@ -131,5 +135,4 @@ public class FeedListFragment extends SherlockDialogFragment implements OnItemCl
 		mFeedListView.setAdapter(adapter);
 
 	}
-
 }
