581cf4294e92c588c8213c1c488ec9c3ab1799b2
quocvule <ledinhquocvu@gmail.com>
2013-12-25 19:12:57 +1030

    #6 Auditor views UI almost done now

--- src/com/cloudjay/cjay/fragment/AuditorDamageQuantityFragment.java
+++ src/com/cloudjay/cjay/fragment/AuditorDamageQuantityFragment.java
@@ -1,36 +1,55 @@
 package com.cloudjay.cjay.fragment;
 
-import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.view.View.OnClickListener;
+import android.app.Activity;
+import android.view.KeyEvent;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.actionbarsherlock.app.SherlockDialogFragment;
 import com.cloudjay.cjay.R;
+import com.cloudjay.cjay.listener.OnReportPageCompleted;
+import com.googlecode.androidannotations.annotations.AfterViews;
 import com.googlecode.androidannotations.annotations.EFragment;
+import com.googlecode.androidannotations.annotations.ViewById;
 
 @EFragment(R.layout.fragment_damage_quantity)
-public class AuditorDamageQuantityFragment extends SherlockDialogFragment implements OnClickListener {
-
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
+public class AuditorDamageQuantityFragment extends SherlockDialogFragment {
+	private int mQuantity;
+	private OnReportPageCompleted mCallback;
+	
+	@ViewById(R.id.quantity) EditText mQuantityEditText;
+	
+	@AfterViews
+	void afterViews() {
+		mQuantityEditText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+			@Override
+			public boolean onEditorAction(TextView textView, int id,
+					KeyEvent keyEvent) {
+				if (id == EditorInfo.IME_ACTION_DONE) {
+					handleReportPageCompleted();
+					return true;
+				}
+				return false;
+			}
+		});
+		mQuantityEditText.requestFocus();
+		mQuantity = 0;
 	}
 	
-	@Override
-	public void onResume() {
-		super.onResume();
-	}
-	
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-		View view = inflater.inflate(R.layout.fragment_damage_quantity, container, false);
-		return view;
-	}
-	
-	@Override
-	public void onClick(View v) {
-		// TODO Auto-generated method stub	
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        try {
+            mCallback = (OnReportPageCompleted) activity;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(activity.toString() + " must implement OnReportPageCompleted");
+        }
+    }
+    
+	private void handleReportPageCompleted() {
+		// Send code to activity, and move to next tab
+		String[] vals = {String.valueOf(mQuantity)};
+		mCallback.onReportPageCompleted(OnReportPageCompleted.TAB_DAMAGE_QUANTITY, vals);
 	}
 }
