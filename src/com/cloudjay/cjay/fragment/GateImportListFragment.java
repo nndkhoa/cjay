fcaef9c051be5d0bd461ca805d13d3ad76a237c8
Hector Zhao <long@greengar.com>
2013-12-26 20:57:59 +0700

    #13: add dialog to show list of operators - Remaining: filter by input

--- src/com/cloudjay/cjay/fragment/GateImportListFragment.java
+++ src/com/cloudjay/cjay/fragment/GateImportListFragment.java
@@ -1,15 +1,19 @@
 package com.cloudjay.cjay.fragment;
 
 import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.Currency;
+
 import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.View.OnKeyListener;
+import android.view.View.OnTouchListener;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
@@ -20,21 +24,21 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
 
 import com.actionbarsherlock.app.SherlockDialogFragment;
 import com.ami.fundapter.BindDictionary;
 import com.ami.fundapter.FunDapter;
 import com.ami.fundapter.extractors.StringExtractor;
 import com.ami.fundapter.interfaces.StaticImageLoader;
-import com.cloudjay.cjay.*;
+import com.cloudjay.cjay.CameraActivity_;
 import com.cloudjay.cjay.R;
 import com.cloudjay.cjay.model.ContainerSession;
+import com.cloudjay.cjay.model.Operator;
 import com.cloudjay.cjay.model.TmpContainerSession;
 import com.cloudjay.cjay.model.User;
 import com.cloudjay.cjay.util.CJayConstant;
 import com.cloudjay.cjay.util.DataCenter;
-
-import com.cloudjay.cjay.util.Mapper;
 import com.cloudjay.cjay.util.Session;
 import com.cloudjay.cjay.util.StringHelper;
 import com.googlecode.androidannotations.annotations.EFragment;
@@ -48,15 +52,27 @@ public class GateImportListFragment extends SherlockDialogFragment implements
 	private Button mAddNewBtn;
 
 	private ListView mFeedListView;
+	private ListView mOperatorListView;
+	private EditText mOperatorEditText;
+	
+	private String containerName;
+	private String operatorName;
 
 	private ArrayList<ContainerSession> mFeeds;
-	private FunDapter<ContainerSession> mAdapter;
+	private FunDapter<ContainerSession> mFeedsAdapter;
+	private ArrayList<Operator> mOperators;
+	private FunDapter<Operator> mOperatorsAdapter;
+	
+	private Dialog mNewContainerDialog;
+	private Dialog mSearchOperatorDialog;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		mFeeds = (ArrayList<ContainerSession>) DataCenter.getInstance()
 				.getListContainerSessions(getActivity());
+		mOperators = (ArrayList<Operator>) DataCenter.getInstance()
+				.getListOperators(getActivity()); 
 	}
 
 	@Override
@@ -69,7 +85,7 @@ public class GateImportListFragment extends SherlockDialogFragment implements
 
 		mFeedListView = (ListView) view.findViewById(R.id.feeds);
 		mFeedListView.setOnItemClickListener(this);
-		initFunDapter(mFeeds);
+		initContainerFeedAdapter(mFeeds);
 
 		return view;
 	}
@@ -79,38 +95,70 @@ public class GateImportListFragment extends SherlockDialogFragment implements
 		super.onResume();
 		
 		mFeeds = (ArrayList<ContainerSession>) DataCenter.getInstance().getListContainerSessions(getActivity());
-		mAdapter.updateData(mFeeds);
+		mFeedsAdapter.updateData(mFeeds);
 	}
 
 	@Override
 	public void onClick(View view) {
-		// Show Dialog
+		showDialogNewContainer();
+	}
+	
+	private void showDialogNewContainer() {
 		LayoutInflater factory = LayoutInflater.from(getActivity());
 		final View newContainerView = factory.inflate(
 				R.layout.dialog_new_container, null);
-
+		
 		final EditText newContainerIdEditText = (EditText) newContainerView
 				.findViewById(R.id.dialog_new_container_id);
+		
+		if (containerName != null) {
+			newContainerIdEditText.setText(containerName);
+		}
+		
 		final Spinner newContainerOwnerSpinner = (Spinner) newContainerView
 				.findViewById(R.id.dialog_new_container_owner);
-
-		ArrayList<String> listOperator = (ArrayList<String>) DataCenter
-				.getInstance().getListOperatorNames(getActivity());
-
-		if (!listOperator.isEmpty()) {
+		
+		if (operatorName != null) {
+			String ops[] = {operatorName};
 			ArrayAdapter<String> adapter = new ArrayAdapter<String>(
-					getActivity(),
-					android.R.layout.simple_spinner_dropdown_item, listOperator);
+					getActivity(), android.R.layout.simple_spinner_item, ops);
+			adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 			newContainerOwnerSpinner.setAdapter(adapter);
-
 		} else {
-			ArrayAdapter<CharSequence> adapter = ArrayAdapter
-					.createFromResource(getActivity(),
-							R.array.dialog_container_owner_list,
-							android.R.layout.simple_spinner_item);
+			String ops[] = {DataCenter.getInstance().getListOperatorNames(getActivity()).get(0)};
+			ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+					getActivity(), android.R.layout.simple_spinner_item, ops);
 			adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 			newContainerOwnerSpinner.setAdapter(adapter);
 		}
+
+		newContainerOwnerSpinner.setOnTouchListener(new OnTouchListener() {
+			
+			@Override
+			public boolean onTouch(View view, MotionEvent event) {
+		        if (event.getAction() == MotionEvent.ACTION_UP) {
+		        	containerName = newContainerIdEditText.getText().toString();
+		        	mNewContainerDialog.dismiss();
+		            showDialogSearchOperator();
+		        }
+		        return true;
+			}
+		});
+		
+		newContainerOwnerSpinner.setOnKeyListener(new OnKeyListener() {
+			
+			@Override
+			public boolean onKey(View view, int keyCode, KeyEvent event) {
+		        if (keyCode == KeyEvent.KEYCODE_DPAD_CENTER) {
+		        	containerName = newContainerIdEditText.getText().toString();
+		        	mNewContainerDialog.dismiss();
+		            showDialogSearchOperator();
+		            return true;
+		        } else {
+		            return false;
+		        }
+			}
+		});
 
 		AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(
 				getActivity())
@@ -120,6 +168,8 @@ public class GateImportListFragment extends SherlockDialogFragment implements
 						new DialogInterface.OnClickListener() {
 							public void onClick(DialogInterface dialog,
 									int whichButton) {
+								
+								containerName = null;
 
 								// Get the container id and container operator
 								// code
@@ -146,32 +196,6 @@ public class GateImportListFragment extends SherlockDialogFragment implements
 								DataCenter.getInstance().setTmpCurrentSession(
 										newTmpContainer);
 
-								// // Create a Container Session
-								// ContainerSession newContainer = Mapper
-								// .toContainerSession(newTmpContainer,
-								// getActivity());
-								//
-								// newContainer.printMe();
-								//
-								// try {
-								// ContainerSessionDaoImpl
-								// containerSessionDaoImpl = DataCenter
-								// .getInstance().getDatabaseManager()
-								// .getHelper(getActivity())
-								// .getContainerSessionDaoImpl();
-								//
-								// containerSessionDaoImpl
-								// .addContainerSessions(newContainer);
-								// } catch (SQLException e) {
-								// e.printStackTrace();
-								// }
-
-								// // Save the current Container Session from
-								// the
-								// // temp Container Session
-								// DataCenter.getInstance().setCurrentSession(
-								// newContainer);
-
 								// Pass tmpContainerSession away
 								// Then start showing the Camera
 								Intent intent = new Intent(getActivity(),
@@ -187,13 +211,81 @@ public class GateImportListFragment extends SherlockDialogFragment implements
 						new DialogInterface.OnClickListener() {
 							public void onClick(DialogInterface dialog,
 									int whichButton) {
-
+								containerName = null;
 							}
 						});
-		dialogBuilder.create().show();
+		mNewContainerDialog = dialogBuilder.create();
+		mNewContainerDialog.show();
+	}
+	
+	private void showDialogSearchOperator() {
+		LayoutInflater factory = LayoutInflater.from(getActivity());
+		final View searchOperatorView = factory.inflate(
+				R.layout.dialog_select_operator, null);
+		
+		mOperatorEditText = (EditText) searchOperatorView.findViewById(R.id.dialog_operator_name);
+		mOperatorListView = (ListView) searchOperatorView.findViewById(R.id.dialog_operator_list);
+		mOperatorListView.setOnItemClickListener(this);
+		initContainerOperatorAdapter(mOperators);
+		
+		if (operatorName != null) {
+			mOperatorEditText.setText(operatorName);
+		}
+		
+		AlertDialog.Builder dialogBuilder = new AlertDialog.Builder(
+				getActivity())
+				.setTitle(getString(R.string.dialog_container_owner))
+				.setView(searchOperatorView)
+				.setPositiveButton(R.string.dialog_container_ok,
+						new DialogInterface.OnClickListener() {
+							public void onClick(DialogInterface dialog,
+									int whichButton) {
+								operatorName = mOperatorEditText.getText().toString();
+								showDialogNewContainer();
+							}
+						})
+				.setNegativeButton(R.string.dialog_container_cancel,
+						new DialogInterface.OnClickListener() {
+							public void onClick(DialogInterface dialog,
+									int whichButton) {
+								showDialogNewContainer();
+							}
+						});
+		mSearchOperatorDialog = dialogBuilder.create();
+		mSearchOperatorDialog.show();
+	}
+	
+	private void initContainerOperatorAdapter(ArrayList<Operator> operators) {
+		BindDictionary<Operator> operatorsDict = new BindDictionary<Operator>();
+		operatorsDict.addStringField(R.id.operator_name,
+				new StringExtractor<Operator>() {
+
+					@Override
+					public String getStringValue(Operator item,
+							int position) {
+						return item.getName();
+					}
+				});
+
+		operatorsDict.addStringField(R.id.operator_code,
+				new StringExtractor<Operator>() {
+
+					@Override
+					public String getStringValue(Operator item,
+							int position) {
+						// TODO Auto-generated method stub
+						return item.getCode();
+					}
+				});
+		
+		mOperatorsAdapter = new FunDapter<Operator>(
+				getActivity(), operators, R.layout.list_item_operator,
+				operatorsDict);
+
+		mOperatorListView.setAdapter(mOperatorsAdapter);
 	}
 
-	private void initFunDapter(ArrayList<ContainerSession> containers) {
+	private void initContainerFeedAdapter(ArrayList<ContainerSession> containers) {
 
 		BindDictionary<ContainerSession> feedsDict = new BindDictionary<ContainerSession>();
 		feedsDict.addStringField(R.id.feed_item_container_id,
@@ -248,21 +340,23 @@ public class GateImportListFragment extends SherlockDialogFragment implements
 					}
 				});
 
-		mAdapter = new FunDapter<ContainerSession>(
+		mFeedsAdapter = new FunDapter<ContainerSession>(
 				getActivity(), containers, R.layout.list_item_container,
 				feedsDict);
 
-		mFeedListView.setAdapter(mAdapter);
+		mFeedListView.setAdapter(mFeedsAdapter);
 
 	}
 
 	@Override
 	public void onItemClick(AdapterView<?> parent, View view, int position,
 			long id) {
-		// Hector: go to details from here
-		// TODO: open new Intent to show CJayImages
-		android.util.Log.d(TAG, "Show item at position: " + position);
-
+		if (view == mFeedListView) {
+			android.util.Log.d(TAG, "Show item at position: " + position);
+		} else {
+			operatorName = mOperators.get(position).getName();
+			mOperatorEditText.setText(operatorName);
+		}
 	}
 
 	@Override
