09a4116e166e368b71001f9aa952fa9821f6c129
Tieu Bao <nntruonghan@gmail.com>
2014-03-20 20:28:32 +0700

    minor changes

--- src/com/cloudjay/cjay/RepairContainerActivity.java
+++ src/com/cloudjay/cjay/RepairContainerActivity.java
@@ -70,7 +70,7 @@ public class RepairContainerActivity extends CJayActivity implements
 				containerSessionDaoImpl.refresh(mContainerSession);
 
 				if (mContainerSession.isValidForUploading()) {
-
+					
 				} else {
 					Crouton.cancelAllCroutons();
 					Crouton.makeText(this, R.string.alert_no_issue_container,
