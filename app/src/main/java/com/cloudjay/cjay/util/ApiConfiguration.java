00ab8ea98da493b77997113c7cf3fc8b50cebdb4
Tieu Bao <nntruonghan@gmail.com>
2014-09-11 15:16:39 +0700

    #188 change target

---
+++ app/src/main/java/com/cloudjay/cjay/util/ApiConfiguration.java
@@ -0,0 +1,8 @@
+package com.cloudjay.cjay.util;
+
+public class ApiConfiguration {
+
+	public static final String ROOT = "https://cloudjay-web.appspot.com";
+	public static final String TOKEN = ROOT + "/api-token-auth/";
+
+}
