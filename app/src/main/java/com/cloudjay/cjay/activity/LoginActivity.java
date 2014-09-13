cc94debccda47af2d1d556e09f9fa90ecad28f02
zzmrthaizz <zzmrthaizz@gmail.com>
2014-09-13 22:03:36 +0700

    #195 need to edit 3 method more

--- app/src/main/java/com/cloudjay/cjay/activity/LoginActivity.java
+++ app/src/main/java/com/cloudjay/cjay/activity/LoginActivity.java
@@ -6,11 +6,14 @@ import android.os.Bundle;
 import android.os.StrictMode;
 import android.util.Log;
 import android.widget.Button;
+import android.widget.EditText;
 
 import com.cloudjay.cjay.R;
 import com.cloudjay.cjay.event.LoginSuccessEvent;
 import com.cloudjay.cjay.model.User;
 import com.cloudjay.cjay.network.NetworkClient;
+import com.cloudjay.cjay.util.Logger;
+import com.squareup.okhttp.internal.Util;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
@@ -21,26 +24,39 @@ public class LoginActivity extends Activity {
 
 	@InjectView(R.id.btn_login)
 	Button mLoginButton;
+	@InjectView(R.id.email)
+	EditText etemail;
+	@InjectView(R.id.password)
+	EditText etpassword;
 
-    @OnClick(R.id.btn_login)
-    void doLogin() {
-        Log.e("OK", "OK");
-        ContentValues values = new ContentValues();
-        values.put(User.ACCESS_TOKEN, "dasdefefdsfdsfdsfs");
-        values.put(User.EMAIL, "giamdinhcong@test.com");
-        values.put(User.ROLE, "full_permission");
-        values.put(User.MOTHER_NAME, "fuck you");
-        values.put(User.FATHER_NAME, "motherfucker");
-        getContentResolver().insert(User.URI, values);
-    }
+	@OnClick(R.id.btn_login)
+	void doLogin() {
+		String email = etemail.getText().toString();
+		String password = etpassword.getText().toString();
+		String token = NetworkClient.getInstance().getToken(this,email, password);
+		Log.e("Results: ", token);
+//		String currentUser = NetworkClient.getInstance().getCurrentUser(this,"Token "+token);
+//		Log.e("Current User: ", currentUser);
+		String autho = "Token "+token;
+		Logger.e(autho);
+		String repairCodes = NetworkClient.getInstance().getRepairCodes(this, autho, "");
+		Log.e("Repair Codes: ", repairCodes);
+		ContentValues values = new ContentValues();
+		values.put(User.ACCESS_TOKEN, "dasdefefdsfdsfdsfs");
+		values.put(User.EMAIL, "giamdinhcong@test.com");
+		values.put(User.ROLE, "full_permission");
+		values.put(User.MOTHER_NAME, "fuck you");
+		values.put(User.FATHER_NAME, "motherfucker");
+		getContentResolver().insert(User.URI, values);
+	}
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_login);
 		ButterKnife.inject(this);
-//		EventBus.getDefault().register(this);
-/*
+		EventBus.getDefault().register(this);
+
 		StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
 				.detectDiskReads()
 				.detectDiskWrites()
@@ -53,25 +69,17 @@ public class LoginActivity extends Activity {
 				.penaltyLog()
 				.penaltyDeath()
 				.build());
-*/
-
-		//NetworkClient.getInstance().getToken("giamdinhcong@test.com", "123456");
-
 
 	}
 
-	/*public void onEvent(LoginSuccessEvent loginSuccessEvent) {
+	public void onEvent(LoginSuccessEvent loginSuccessEvent) {
+		Log.e("EventBus: ", "OK");
 
-
-	}*/
-
-//	public void onEventLoginSuccessEvent() {
-//		Log.i("OK", "OK");
-//	}
+	}
 
 	@Override
 	protected void onDestroy() {
 		super.onDestroy();
-	//	EventBus.getDefault().unregister(this);
+		//	EventBus.getDefault().unregister(this);
 	}
 }
--- app/src/main/java/com/cloudjay/cjay/util/ApiEndpoint.java
+++ app/src/main/java/com/cloudjay/cjay/util/ApiEndpoint.java
@@ -5,13 +5,13 @@ public class ApiEndpoint {
 	public static final String ROOT_API = "https://cloudjay-web.appspot.com";
 	public static final String TOKEN_API = "/api-token-auth/";
 	public static final String API_ADD_GCM_DEVICE_API = "/mobile/gcm-devices.json";
-	public static final String CURRENT_USER_API = "/cjay/current-user.json";
-	public static final String LIST_OPERATORS_API = "/cjay/container-operators.json";
-	public static final String LIST_DAMAGE_CODES_API = "/cjay/damage-codes.json";
-	public static final String LIST_REPAIR_CODES_API = "/cjay/repair-codes.json";
-	public static final String LIST_COMPONENT_CODES_API = "/cjay/component-codes.json";
-	public static final String CONTAINER_SESSIONS_API = "/cjay/container-sessions.json";
-	public static final String CONTAINER_SESSION_ITEM_API = "/cjay/container-sessions/%s.json";
+	public static final String CURRENT_USER_API = "/api/cjay/current-user.json";
+	public static final String LIST_OPERATORS_API = "/api/cjay/container-operators.json";
+	public static final String LIST_DAMAGE_CODES_API = "/api/cjay/damage-codes.json";
+	public static final String LIST_REPAIR_CODES_API = "/api/cjay/repair-codes.json";
+	public static final String LIST_COMPONENT_CODES_API = "/api/cjay/component-codes.json";
+	public static final String CONTAINER_SESSIONS_API = "/api/cjay/container-sessions.json";
+	public static final String CONTAINER_SESSION_ITEM_API = "/api/cjay/container-sessions/%s.json";
 
 //	public static final String CJAY_TMP_STORAGE = "https://www.googleapis.com/upload/storage/v1beta2/b/cjaytmp/o?uploadType=media&name=%s";
 	public static final String CJAY_TMP_STORAGE = "https://www.googleapis.com/upload/storage/v1beta2/b/cjaytmp/o";
