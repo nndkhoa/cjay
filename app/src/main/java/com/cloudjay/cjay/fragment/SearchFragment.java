0fcb9f1217ad41fccc1afc81c685121ae6f06021
nambv <nambv@outlook.com>
2014-10-02 02:30:07 +0700

    #197 change actionbar navigation tabs in home activity

--- app/src/main/java/com/cloudjay/cjay/fragment/SearchFragment.java
+++ app/src/main/java/com/cloudjay/cjay/fragment/SearchFragment.java
@@ -1,18 +1,41 @@
 package com.cloudjay.cjay.fragment;
 
+
+
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.cloudjay.cjay.R;
+
 /**
- * Created by Thai on 9/30/2014.
+ * A simple {@link Fragment} subclass.
+ *
  */
+public class SearchFragment extends Fragment {
 
-public class SearchFragment extends android.support.v4.app.Fragment{
+    private static final String ARG_SECTION_NUMBER = "section_number";
 
-	@Override
-	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-		return super.onCreateView(inflater, container, savedInstanceState);
-	}
+    public SearchFragment() {
+        // Required empty public constructor
+    }
+
+    public static SearchFragment newInstance(int sectionNumber) {
+        SearchFragment fragment = new SearchFragment();
+        Bundle args = new Bundle();
+        args.putInt(ARG_SECTION_NUMBER, sectionNumber);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_search, container, false);
+    }
+
+
 }
