75008573b4ad341463e6711ab473e9447aa69d69
Tieu Bao <nntruonghan@gmail.com>
2014-10-07 09:00:59 +0700

    add RealmSessionDataLoader.java

--- app/src/main/java/com/cloudjay/cjay/api/NetworkClient.java
+++ app/src/main/java/com/cloudjay/cjay/api/NetworkClient.java
@@ -230,22 +230,12 @@ public class NetworkClient {
 		if (id != result.get("id").getAsLong()) {
 			return null;
 		} else {
-			// WTF is this shit :| Fuck Realm
+
 			Realm realm = Realm.getInstance(context);
 			realm.beginTransaction();
 			Session session = realm.createObject(Session.class);
 
-			User user = realm.createObject(User.class);
-			user.setId(result.get("id").getAsLong());
-			user.setFirstName(result.get("first_name").getAsString());
-			user.setLastName(result.get("last_name").getAsString());
-			user.setUsername(result.get("username").getAsString());
-			user.setEmail(result.get("email").getAsString());
-			user.setFullName(result.get("full_name").getAsString());
-			user.setRole(result.get("role").getAsLong());
-			user.setRoleName(result.get("role_name").getAsString());
-			user.setDepotCode(result.get("depot_code").getAsString());
-			user.setAvatarUrl(result.get("avatar_url").getAsString());
+			// TODO: update session values
 
 			realm.commitTransaction();
 			return session;
--- app/src/main/java/com/cloudjay/cjay/fragment/WorkingFragment.java
+++ app/src/main/java/com/cloudjay/cjay/fragment/WorkingFragment.java
@@ -1,14 +1,39 @@
 package com.cloudjay.cjay.fragment;
 
+import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.Loader;
 
 import com.cloudjay.cjay.R;
+import com.cloudjay.cjay.model.Session;
 
 import org.androidannotations.annotations.EFragment;
 
+import io.realm.RealmResults;
+
 @EFragment(R.layout.fragment_working)
-public class WorkingFragment extends Fragment {
-    public WorkingFragment() {
+public class WorkingFragment extends Fragment implements LoaderManager.LoaderCallbacks<RealmResults<Session>> {
+
+	private static final int LOADER_ID = 1;
+
+	public WorkingFragment() {
         // Required empty public constructor
     }
+
+	@Override
+	public Loader<RealmResults<Session>> onCreateLoader(int id, Bundle args) {
+		return null;
+	}
+
+	@Override
+	public void onLoadFinished(Loader<RealmResults<Session>> loader, RealmResults<Session> data) {
+
+	}
+
+	@Override
+	public void onLoaderReset(Loader<RealmResults<Session>> loader) {
+
+	}
+
 }
