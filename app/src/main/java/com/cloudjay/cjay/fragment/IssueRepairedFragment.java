c66fbe07e9a64591e0b0f39ae73ca4579e482871
nambv <nambv@outlook.com>
2014-10-14 16:48:12 +0700

    #198 fix bug missing tab content fragment in RepairFragment

--- app/src/main/java/com/cloudjay/cjay/fragment/IssueRepairedFragment.java
+++ app/src/main/java/com/cloudjay/cjay/fragment/IssueRepairedFragment.java
@@ -14,18 +14,8 @@ import org.androidannotations.annotations.EFragment;
 @EFragment(R.layout.fragment_issue_repaired)
 public class IssueRepairedFragment extends Fragment {
 
-	private static final String ARG_SECTION_NUMBER = "section_number";
-
 	public IssueRepairedFragment() {
 		// Required empty public constructor
-	}
-
-	public static IssueRepairedFragment newInstance(int sectionNumber) {
-		IssueRepairedFragment fragment = new IssueRepairedFragment();
-		Bundle args = new Bundle();
-		args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-		fragment.setArguments(args);
-		return fragment;
 	}
 
 }
