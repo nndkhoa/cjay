31b1ce0b0209dfb8c5149d9d6e88a7aa50164a9b
nambv <nambv@outlook.com>
2014-10-31 18:41:27 +0700

    #212 minor change in issue pending

--- app/src/main/java/com/cloudjay/cjay/fragment/IssuePendingFragment.java
+++ app/src/main/java/com/cloudjay/cjay/fragment/IssuePendingFragment.java
@@ -160,7 +160,6 @@ public class IssuePendingFragment extends Fragment {
         // Open camera activity
         Intent cameraActivityIntent = new Intent(getActivity(), CameraActivity_.class);
         cameraActivityIntent.putExtra(CameraFragment.CONTAINER_ID_EXTRA, containerID);
-        cameraActivityIntent.putExtra(CameraFragment.IMAGE_TYPE_EXTRA, ImageType.AUDIT.value);
         cameraActivityIntent.putExtra(CameraFragment.OPERATOR_CODE_EXTRA, operatorCode);
         cameraActivityIntent.putExtra(CameraFragment.CURRENT_STEP_EXTRA, Step.AUDIT.value);
         startActivity(cameraActivityIntent);
