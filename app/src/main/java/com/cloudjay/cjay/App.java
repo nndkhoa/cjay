e5118ae9852db5bd4f37c2b72211d0e4320fa45d
nambv <nambv@outlook.com>
2014-11-14 16:55:06 +0700

    #217 set default options for setting in application class

--- app/src/main/java/com/cloudjay/cjay/App.java
+++ app/src/main/java/com/cloudjay/cjay/App.java
@@ -3,7 +3,9 @@ package com.cloudjay.cjay;
 import android.app.Application;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Bitmap;
+import android.preference.PreferenceManager;
 import android.util.Log;
 
 import com.cloudjay.cjay.task.service.PubnubService_;
@@ -74,6 +76,7 @@ public class App extends Application {
 		configureImageLoader();
 		configureJobManager();
 		configureAlarmManager();
+        configureSettings();
 
 //		StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder().detectAll().penaltyLog().build());
 //		StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder().detectAll().build());
@@ -203,6 +206,40 @@ public class App extends Application {
 
 		jobManager = new JobManager(this, configuration);
 	}
+
+    /**
+     *  Cấu hình cho Setting
+     */
+    private void configureSettings() {
+        boolean defaultDebug = true;
+        boolean defaultAutoUpdate = true;
+        boolean defaultRainyMode = false;
+
+        // Configure Logger
+        boolean debuggable = PreferenceManager.getDefaultSharedPreferences(getApplicationContext())
+                .getBoolean(getString(R.string.pref_key_enable_logger_checkbox),
+                        defaultDebug);
+        Logger.getInstance().setDebuggable(debuggable);
+
+        // Configure AutoUpdate
+        boolean autoUpdate = PreferenceManager.getDefaultSharedPreferences(getApplicationContext())
+                .getBoolean(getString(R.string.pref_key_enable_logger_checkbox),
+                        defaultAutoUpdate);
+
+        // Configure rainy mode
+        boolean rainyMode = PreferenceManager.getDefaultSharedPreferences(getApplicationContext())
+                .getBoolean(getString(R.string.pref_key_enable_temporary_fragment_checkbox),
+                        defaultRainyMode);
+
+        SharedPreferences.Editor editor =
+                PreferenceManager.getDefaultSharedPreferences(getApplicationContext()).edit();
+        editor.putBoolean(getString(R.string.pref_key_enable_logger_checkbox), debuggable);
+        editor.putBoolean(getString(R.string.pref_key_auto_check_update_checkbox), autoUpdate);
+        editor.putBoolean(getString(R.string.pref_key_enable_temporary_fragment_checkbox), rainyMode);
+        editor.commit();
+
+
+    }
 
 	private static JobManager jobManager;
 
